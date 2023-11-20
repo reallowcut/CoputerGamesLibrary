c5fb34ad2bed77c043b45e704e5688161ade81d1
drogan <droganpythonpro@gmail.com>
2023-11-20 13:10:20 +0300

    add login.html and registration.html bootstrap ship

--- src/main/java/com/aply/application/sptingapplication/SptingApplicationAplyApplication.java
+++ src/main/java/com/aply/application/sptingapplication/SptingApplicationAplyApplication.java
@@ -2,6 +2,9 @@ package com.aply.application.sptingapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SptingApplicationAplyApplication {
@@ -9,5 +12,6 @@ public class SptingApplicationAplyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SptingApplicationAplyApplication.class, args);
 	}
+
 
 }
